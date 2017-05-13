d20c8e7c50536de9eac0855df00216f5ddd9fe70
Neamar <neamar@neamar.fr>
2017-05-13 11:33:38 +0100

    Start implementing a generic helper to test images

--- app/src/androidTest/java/fr/neamar/panelbypanel/PanelAnalyzerTest.java
+++ app/src/androidTest/java/fr/neamar/panelbypanel/PanelAnalyzerTest.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
+import android.support.annotation.DrawableRes;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -23,14 +24,40 @@ import static junit.framework.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class PanelAnalyzerTest {
-    @Test
-    public void computeCorrectPanels() throws Exception {
-        // Context of the app under test.
+    /**
+     * Test t hat an image is correctly analyzed
+     *
+     * @param drawable             the drawable to analyzed
+     * @param expectedPanelsByTier the expected panel distribution -- for instance 3,2,1 for an image with a first panel comprising 3 tiers, the second panel being made up of 2 and the last being a single tier.
+     */
+    private void testResource(@DrawableRes int drawable, int[] expectedPanelsByTier) {
         Context appContext = InstrumentationRegistry.getTargetContext();
-        Bitmap bitmap = BitmapFactory.decodeResource(appContext.getResources(), R.drawable.sample);
+        Bitmap bitmap = BitmapFactory.decodeResource(appContext.getResources(), drawable);
         PanelAnalyzer panelAnalyzer = new PanelAnalyzer(bitmap);
         ArrayList<Rect> panels = panelAnalyzer.getPanels();
 
-        assertEquals(12, panels.size());
+        ArrayList<ArrayList<Rect>> tiers = new ArrayList<>();
+        ArrayList<Rect> currentTier = new ArrayList<>();
+
+        int currentY = panels.get(0).top;
+        for(Rect panel: panels) {
+            if(panel.top == currentY) {
+                currentTier.add(panel);
+            }
+            else {
+                tiers.add(currentTier);
+                currentY = panel.top;
+                currentTier = new ArrayList<>();
+            }
+        }
+        // Add the last panel
+        tiers.add(currentTier);
+
+        assertEquals("Invalid tier number,", expectedPanelsByTier.length, tiers.size());
+    }
+
+    @Test
+    public void computeCorrectPanels() throws Exception {
+        testResource(R.drawable.sample, new int[]{2, 3, 2, 2});
     }
 }
