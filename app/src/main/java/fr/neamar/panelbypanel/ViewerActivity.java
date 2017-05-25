499e9557edbc22def89ea278cf99588dfe6f7b12
Neamar <neamar@neamar.fr>
2017-05-25 17:12:56 +0100

    Created structure with book interface

--- app/src/main/java/fr/neamar/panelbypanel/ViewerActivity.java
+++ app/src/main/java/fr/neamar/panelbypanel/ViewerActivity.java
@@ -1,7 +1,6 @@
 package fr.neamar.panelbypanel;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,12 +10,20 @@ import android.view.View;
 
 import java.util.ArrayList;
 
+import fr.neamar.panelbypanel.comic.Book;
+import fr.neamar.panelbypanel.comic.SampleBook;
 import fr.neamar.panelbypanel.panel.PanelAnalyzer;
+
+import static fr.neamar.panelbypanel.R.id.page;
 
 public class ViewerActivity extends AppCompatActivity {
     private static final boolean DEBUG = true;
 
     private static final String TAG = "ViewerActivity";
+
+    private Book currentBook;
+    private int currentPageNumber = 0;
+
     private int currentPanelNumber = 0;
     private ArrayList<Rect> panels;
     private PanelImageView panelImageView;
@@ -26,24 +33,42 @@ public class ViewerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewer);
 
-        panelImageView = (PanelImageView) findViewById(R.id.page);
+        panelImageView = (PanelImageView) findViewById(page);
 
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inMutable = DEBUG;
+        loadBook(new SampleBook(getResources(), DEBUG));
 
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sample, options);
-        PanelAnalyzer panelAnalyzer = new PanelAnalyzer(bitmap, true);
-        panels = panelAnalyzer.getPanels();
-        // panelAnalyzer.colorizeBackground();
-
-        panelImageView.setImageBitmap(bitmap);
         panelImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 moveToNextPanel();
             }
         });
+    }
 
+    protected void loadBook(Book book) {
+        currentBook = book;
+        currentPageNumber = 0;
+        currentPanelNumber = 0;
+        moveToPage();
+    }
+
+    /**
+     * Move to page currentPageNumber
+     */
+    protected void moveToPage() {
+        if(currentPageNumber >= currentBook.getPageCount()) {
+            throw new RuntimeException("You've reached the end of the book");
+        }
+
+        Bitmap bitmap = currentBook.getPage(currentPageNumber);
+        currentPanelNumber = 0;
+        PanelAnalyzer panelAnalyzer = new PanelAnalyzer(bitmap, true);
+        panels = panelAnalyzer.getPanels();
+        // panelAnalyzer.colorizeBackground();
+
+        panelImageView.setImageBitmap(bitmap);
+
+        // Move to first panel
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -53,11 +78,30 @@ public class ViewerActivity extends AppCompatActivity {
         }, 100);
     }
 
+    /**
+     * Move to the next panel, or next page if no panels left to read
+     */
     protected void moveToNextPanel() {
-        Rect panel = panels.get(currentPanelNumber % panels.size());
+        currentPanelNumber += 1;
+        if(currentPanelNumber >= panels.size()) {
+            currentPageNumber += 1;
+            moveToPage();
+            return;
+        }
+
+        moveToPanel();
+    }
+
+    /**
+     * Move to panel currentPanelNumber on currentPageNumber
+     */
+    protected void moveToPanel() {
+        if(currentPanelNumber >= panels.size()) {
+            throw new RuntimeException("Trying to move to non existing panel");
+        }
+
+        Rect panel = panels.get(currentPanelNumber);
         Log.i(TAG, "Moving to " + panel);
         panelImageView.goToPanel(panel);
-
-        currentPanelNumber += 1;
     }
 }
---
+++ app/src/main/java/fr/neamar/panelbypanel/comic/Book.java
@@ -0,0 +1,12 @@
+package fr.neamar.panelbypanel.comic;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by neamar on 25/05/17.
+ */
+
+public interface Book {
+    public int getPageCount();
+    public Bitmap getPage(int i);
+}
